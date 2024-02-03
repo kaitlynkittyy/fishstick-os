4b74d0f84383a823eb9f3396c14107f698d73353
Kaitlyn <87152313+kaitlynkittyy@users.noreply.github.com>
2024-02-03 00:53:28 -0500

    worked on buff woman (buff-her)

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,7 @@ fn panic(_info: &PanicInfo) -> ! {
     loop{}
 }
 
-static OUTPUT: &[u8] = b"ERROR";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buf = 0xb8000 as *mut u8;
-    for (i, &byte) in OUTPUT.iter().enumerate() {
-        unsafe {
-            *vga_buf.offset(i as isize * 2) = byte;
-            *vga_buf.offset(i as isize * 2 + 1) = 0xCF;
-        }
-    }
     loop{}
 }
--- src/vga_buf.rs
+++ src/vga_buf.rs
@@ -1,3 +1,6 @@
+use volatile::Volatile;
+use core::fmt;
+
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
@@ -42,5 +45,63 @@ const BUFFER_WIDTH: usize = 80;
 
 #[repr(transparent)]
 struct Buffer {
-    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+pub struct Writer {
+    column_pos: usize,
+    color_code: ColorCode,
+    buffer: &'static mut Buffer,
+}
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+            if self.column_pos >= BUFFER_WIDTH {
+                self.new_line();
+            }
+
+            let row = BUFFER_HEIGHT - 1;
+            let col = self.column_pos;
+
+            let color_code = self.color_code;
+            self.buffer.chars[row][col].write(ScreenChar {
+                ascii_char: byte,
+                color_code,
+            });
+            self.column_pos += 1;
+            }
+        }
+    }
+
+    fn new_line(&mut self) {
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_pos = 0;
+    }
+    
+    fn clear_row(&mut self, row: usize) {/*TODO*/}
+
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                _=> self.write_byte(0xfe),
+            }
+        }
+    }
+}
+
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
 }
